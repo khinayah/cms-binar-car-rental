3f16ec3a795fc8547bdf2b875b1234133327067e
korundum1001 <madanebook@gmail.com>
2023-07-29 19:25:30 +0800

    make route path to admin add cars page

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import Layout from "./layout/Main";
+import AdminAddCar from "./pages/AdminAddCar";
 
 function App() {
   let element = useRoutes ([
     {path: "/", element: <AdminLogin/>},
-    {path: "/dashboard", element: <Layout/>}
+    {path: "/dashboard", element: <Layout/>},
+    {path: "/add-cars", element:<AdminAddCar/>}
   ])
 
   return element
