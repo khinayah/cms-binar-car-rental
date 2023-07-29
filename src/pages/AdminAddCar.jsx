9b330b55c510a195c7a086d8ca46c434bdd00dd7
korundum1001 <madanebook@gmail.com>
2023-07-29 14:13:41 +0800

    create template for Admin Add Car Page

---
+++ src/pages/AdminAddCar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./csspages/AdminAddCar.css"
+
+const AdminAddCar = () => {
+
+    return (
+        <div>Ini Halaman Add Car</div>
+    )
+}
+
+export default AdminAddCar;
