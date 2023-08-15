22d618d1999f9bef278fa3e420d4852145c51fb1
korundum1001 <madanebook@gmail.com>
2023-08-15 22:07:28 +0800

    resolve the conflict

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 const AdminChart = () => {
+  const [data, setData] = useState([]);
+  
 
+  const getData = () => {
+    const config = {
+      headers: {
+          access_token : localStorage.getItem('admin_token')
+      }
+  }
+
+    let apiUrl = `https://api-car-rental.binaracademy.org/admin/v2/order`
+
+    axios
+      .get(apiUrl, config)
+      .then((res) => setData(res.data.cars))
+      .catch((err) => setErr(err.message));
+
+    // setTimeout(() => {
+    //   setErr("");
+    // }, 4000);
+  }
 
     return ( 
     <>
